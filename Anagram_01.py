40e953ead94737899cddd752b7ea4155299e86db
Rei-0a <spiraea.0a@gmail.com>
2025-05-24 00:44:15 +0900

    Sort Dictionary

--- Anagram_01.py
+++ Anagram_01.py
@@ -3,8 +3,23 @@
 
 # 辞書ファイルの取得、1行ごとに保存
 f = open('words.txt','r')
-data = f.readlines()    # 1行ごとにデータを取得
+data = f.read().splitlines()    # 1行ごとにデータを取得。辞書データの\nは除去。
 f.close()
 
-def searchWord(  ):
+# 新しい辞書を作成する関数
+def createSortedDictionary( words ):
 
+    sortedDictionary = []
+
+    for i in words:
+        sortedWord = ''.join(sorted(i))    # iをソートしたものを結合する
+        sortedDictionary.append( sortedWord , i )    # (ソート済み単語、元の単語)をアペンド
+
+    sortedDictionary = sorted(sortedDictionary) # 単語内をソートした辞書をさらにソート
+
+    return sortedDictionary
+
+
+createSortedDictionary(data)
+
+    
