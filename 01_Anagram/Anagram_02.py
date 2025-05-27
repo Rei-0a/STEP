97b2d079951a294a6a1c14ab04f3bb5d818a7f77
Rei-0a <spiraea.0a@gmail.com>
2025-05-28 01:31:18 +0900

    Added score to each dictionary entry and sorted by score to significantly improve calculation speed.

--- 01_Anagram/Anagram_02.py
+++ 01_Anagram/Anagram_02.py
@@ -1,26 +1,13 @@
 '''
-与えられた文字列の全ての文字を使わなくても良いように関数をアップグ
-レードする。
+与えられた文字列の全ての文字を使わなくても良いように関数をアップグレードする。
 入力：small.txt , medium.txt, large.txt
-出力：各単語について「最大のスコアを持つアナグラム」を列挙したファイ
-ル
-'''
+出力：各単語について「最大のスコアを持つアナグラム」を列挙したファイル
+単語のスコアは、文字によって点数が変わる
+    onePointChar = ['a','e','h','i','n','o','r','s','t']
+    twoPointChar = ['c','d','l','m','u']
+    threePointChar = ['b','f','g','p','v','w','y']
+    fourPointChar = ['j','k','q','x','z']
 
-'''
-聞きたいこと
-スコア計算をどこでやるのが最もいいのか？
-<calculateBestScore内>
-全ての解答が入った配列を引数として計算している
-×再度どのアルファベットが使用されているか一字ずつ見てしまっている
-〇答えではないとわかっている単語のスコアを計算しなくともよい
-
-N回の計算量？
-
-<anagramSearch内>
-〇単語のアルファベットの数を予め計算していて、その数をもとに判定している
-×アナグラムを作成できない単語のスコアも途中までではあるけれど計算してしまうことになる
-
-（存在するアナグラムの数）× （その文字数）の計算量？
 '''
 
 import re
@@ -40,97 +27,54 @@ f = open('words.txt','r')
 data = f.read().splitlines()    # 1行ごとにデータを取得。辞書データの\nは除去。
 f.close()
 
-# 新しい辞書(それぞれの単語をアルファベットを数えたもの)を作成する関数
+# 新しい辞書(それぞれの単語のアルファベットを数えたもの、元の単語、スコア)を作成する関数
 def createCountedDictionary( words ):
 
-    countedDictionary = []
-
-    # sortedf = open('sortedWords_02.txt','w')
+    countedDictionary = []    
+    Point = [1,3,2,2,1,3,3,1,1,4,4,2,2,1,1,3,4,1,1,1,2,3,3,4,3,4]
 
     for word in words:
         alphabetsCount = [0]*26
+        score = 0
         for j in range(len(word)):
-            alphabetsCount[(ord(word[j])-ord('a'))] += 1    # a:0 ~ z:25　の場所をインクリメント
+            alphabetsCount[(ord(word[j])-ord('a'))] += 1    # a:0 ~ z:25 の場所をインクリメント
+            score += Point[(ord(word[j])-ord('a'))]         # wordのスコアも計算しておく
         
-        alphabetsCount.append(word)
+        alphabetsCount.append(word)     # 元の単語をアペンド
+        alphabetsCount.append(score)    # その単語が持つスコアをアペンド
         countedDictionary.append(alphabetsCount)
-        # sortedf.write(str(alphabetsCount)+"\n")
+        
+    countedDictionary=sorted(countedDictionary, key= lambda x: x[27], reverse=True) # 単語が持つ点数が高い順にソートする
 
-    # sortedf.close()
     return countedDictionary
 
 # アルファベットをカウントした単語(input) が、辞書(Words)の中にあるかを調べていく関数
 def anagramSearch( input , Words):
     anagramSets= []
-    
+        
     for i in range(len(Words)):
-        canCreate = 1   # Words[i][:]が作れるときは1、そうでなければ0
+        canCreate = True   # Words[i][:]が作れるときはTrue
         
         for j in range(26):
-            if( input[j] < Words[i][j] and canCreate == 1):
-                canCreate = 0
+            if( input[j] < Words[i][j] ):
+                canCreate = False
                 break
             else:
-                canCreate = 1
+                canCreate = True
         
-        if(canCreate == 1):
+        if(canCreate == True):
             anagramSets.append(Words[i][26])
+            return Words[i][:]
                 
-    return anagramSets
-
-# 答えを全て出力する関数
-def printAllAnswers( Answers ):
-
-    if(Answers == []):
-        print("There are no anagrams that can be made from"+seikika+"'s characters.")
-    else:
-        print("The anagrams that can be made from"+seikika+"'s these characters are as follows:")
-        print('length = ',len(Answers))
-        for i in range(len(Answers)):
-            if(i < len(Answers)-1):
-                print(Answers[i],end=', ')
-            else:
-                print(Answers[i],end = '\n\n')
-
-# 最も良いスコアを全て計算する関数
-def calculateBestScore( Answers ):
-
-    bestScore = 0
-    bestScoreWord = ''
-    onePointChar = ['a','e','h','i','n','o','r','s','t']
-    twoPointChar = ['c','d','l','m','u']
-    threePointChar = ['b','f','g','p','v','w','y']
-    fourPointChar = ['j','k','q','x','z']
-
-    for word in range(len(Answers)):
-        score = 0
-        # print(Answers[word])
-        for i in range(len(Answers[word])):
-            
-            if(Answers[word][i] in onePointChar):
-                score += 1
-            elif(Answers[word][i] in twoPointChar):
-                score += 2
-            elif(Answers[word][i] in threePointChar):
-                score += 3
-            elif(Answers[word][i] in fourPointChar):
-                score += 4
-
-        if(score > bestScore):
-            bestScore = score
-            bestScoreWord = word
-
-    # print(Answers[bestScoreWord], bestScore)
-
-    return Answers[bestScoreWord]
-
-            
+    # return anagramSets
 
 countedDictionary = createCountedDictionary(data)    # 辞書の単語のアルファベットをカウントする
 
+# 入力データの読み込み
 f = open('large.txt','r')
 inputData = f.read().splitlines()
 f.close
+
 
 outputfile = open('result.txt','w')
 
@@ -153,15 +97,36 @@ for input in range(len(inputData)):
 
     Answers = anagramSearch(inputAlphabetCount,countedDictionary)   # 答えを格納する
 
-    # printAllAnswers(Answers)
-    bestWord = calculateBestScore(Answers)
-    outputfile.write(bestWord+'\n')
+    outputfile.write(Answers[26]+'\n')  # 最もよいスコアをファイルへ記入
+
 
 outputfile.close()
 
 '''
-small.txt score 193
+MY SCORE
+small.txt 193
 medium.txt 18911
 large.txt 244642
 large.txtのbestアナグラムを出力するときに、25秒で513個ほどの計算量。8分くらい待つ。遅すぎる、、、？
+→辞書内にスコアを格納して、点数で辞書をソートする形式へ変更すると10秒ほどの実行時間になった(5/28)
+'''
+
+'''
+聞きたいこと(5/28)
+
+スコア計算をどこでやるのが最もいいのか？
+<calculateBestScore内>
+全ての解答が入った配列を引数として計算している
+×再度どのアルファベットが使用されているか一字ずつ見てしまっている
+〇答えではないとわかっている単語のスコアを計算しなくともよい
+
+N回の計算量？
+
+<anagramSearch内>
+〇単語のアルファベットの数を予め計算していて、その数をもとに判定している
+×アナグラムを作成できない単語のスコアも途中までではあるけれど計算してしまうことになる
+
+（存在するアナグラムの数）× （その文字数）の計算量？
+
+→今回最適なのは、辞書内に配置すること！！実行時間もとても早くなった！！
 '''
