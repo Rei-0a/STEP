a21e5d4256f00c92b9bace6a21526885971c5ee9
Rei-0a <spiraea.0a@gmail.com>
2025-07-09 18:04:18 +0900

    complete free bin list

--- 07_HowProgramWorks/README.md
+++ 07_HowProgramWorks/README.md
@@ -40,7 +40,11 @@
 
 ## 4. 提案する設計（Proposed Design）
 
+Best-Fit型にして、FreeListBinにした。
+
 ### 4.1 Best-Fit malloc
+
+最後まで`metadata`を確認して、必要な`size`よりも大きい`metadata->size`を持つ中で最小の領域(適切な領域)を探す。
 
 ```
 // Best-fit
@@ -59,6 +63,27 @@
   prev = best_prev; // 最も良いmetadataにする。
   metadata = best_metadata;
 ```
+
+### 4.2 Free List Bin
+構造体を
+```
+typedef struct my_heap_t {
+  my_metadata_t *free_head[N];
+  my_metadata_t dummy;
+} my_heap_t;
+```
+のように書き換えて、free_headを`N`個にした。(つまり、ビンの数を`N`個にした。)
+各ビンが格納するメモリのサイズは
+```C
+bin_size_start[N] = {0,100,300,500,700,900,1000,1200};
+```
+とした。
+
+その後、`metadata`のサイズが入る`bin`のインデックスを計算し(`caluculate_bin_index`関数)した。それ以上のインデックスを持つ`bin`を探索し、`free size list`から`bestfit`を用いて空いているメモリを探した。
+
+
+
+### 4.2 
 
 ## 5. 結果
 
@@ -89,9 +114,36 @@ Please copy & paste the following data in the score sheet!
 16,70,23,39,258,9,91900,16,92859,15,
 ```
 
+Challenge 1
+全て128
+Challenge 2
+ほとんど2桁のメモリを要求している
+Challenge 3
+同じ
+Challenge 4
+ほとんど3桁のメモリを要求している (4桁含む)
+Challenge 4
+2桁~4桁までランダムにメモリを要求している
 
+
+##### (0) First Fit
+
+##### (1) Best Fitの実装
+(0)と比較して、Utilicationは全てのChallengeで改善された。これは、最小限のブロックを使うことになり、結果として、断片化が抑えられているからであると考えられる。
+また、Challenge 1,2,3は実行時間が伸びたが、Challenge 4,5は実行時間が短縮された。これは、Challenge1~3は、必要なメモリが小さいため、すぐに見つけたところにいれても、他のメモリの確保にあまり影響を与えないため、実行時間が伸びたのではないかと考えられる。
+
+##### (2) Free List Binの実装
+##### ()
+##### ()
 
 ## 5. 代替案の検討（Alternatives Considered）
+
+##### Free-List-Binの各`bin`が担当するデータサイズの範囲
+- (1)`int bin_size_start[N] = {0,100,300,500,700,900,1000,1200};`のとき
+- (2)`int bin_size_start[N] = {0,50,100,300,500,700,900,1000,1200,1400};`のとき
+`N=10`としたが、`N=8`のときよりも時間がかかってしまった。
+
+
 
 
 <!-- 
--- 07_HowProgramWorks/malloc_challenge/malloc/malloc.c
+++ 07_HowProgramWorks/malloc_challenge/malloc/malloc.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define N 8 // binの数
 
 //
 // Interfaces to get memory pages from OS
@@ -31,7 +32,7 @@ typedef struct my_metadata_t {
 } my_metadata_t;
 
 typedef struct my_heap_t {
-  my_metadata_t *free_head;
+  my_metadata_t *free_head[N];
   my_metadata_t dummy;
 } my_heap_t;
 
@@ -44,19 +45,30 @@ my_heap_t my_heap;
 // Helper functions (feel free to add/remove/edit!)
 //
 
-void my_add_to_free_list(my_metadata_t *metadata) {
+void my_add_to_free_list(my_metadata_t *metadata, int bin_index) {
   assert(!metadata->next);
-  metadata->next = my_heap.free_head;
-  my_heap.free_head = metadata;
+  metadata->next = my_heap.free_head[bin_index];
+  my_heap.free_head[bin_index] = metadata;
 }
 
-void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
+void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev, int bin_index) {
   if (prev) {
     prev->next = metadata->next;
   } else {
-    my_heap.free_head = metadata->next;
+    my_heap.free_head[bin_index] = metadata->next;
   }
   metadata->next = NULL;
+}
+
+// sizeが与えられたときに、それがどのビンに入るかを返す関数
+int calculate_bin_index(size_t size){
+  int bin_size_start[N] = {0,100,300,500,700,900,1000,1200};  // 各indexのbinの最小値が入っている配列
+  for( int i = 0 ; i < N ; i++ ){
+    if(size <= bin_size_start[i]){  // i番目のbinでは大きすぎるとき
+      return i-1;
+    }
+  }
+  return N-1; // 一番大きいときは、最後のbinに入ってもらう
 }
 
 //
@@ -65,7 +77,9 @@ void my_remove_from_free_list(my_metadata_t *metadata, my_metadata_t *prev) {
 
 // This is called at the beginning of each challenge.
 void my_initialize() {
-  my_heap.free_head = &my_heap.dummy;
+  for( int i = 0 ; i < N ; i++ ){
+    my_heap.free_head[i] = &my_heap.dummy;
+  }
   my_heap.dummy.size = 0;
   my_heap.dummy.next = NULL;
 }
@@ -75,29 +89,44 @@ void my_initialize() {
 // 4000. You are not allowed to use any library functions other than
 // mmap_from_system() / munmap_to_system().
 void *my_malloc(size_t size) {
-  my_metadata_t *metadata = my_heap.free_head;
+  if (size == 0){
+    return NULL;
+  }
+  int bin_index = calculate_bin_index(size);  // sizeが確保できそうなbin_inexを探す
+  my_metadata_t *metadata = my_heap.free_head[bin_index]; // そのbinの中から、確保するメモリを決める
   my_metadata_t *prev = NULL;
+
   // First-fit: Find the first free slot the object fits.
   // TODO: Update this logic to Best-fit!
   // while (metadata && metadata->size < size) { // sizeの入ることができるmetadataの場所を探すまでループ
   //   prev = metadata;
   //   metadata = metadata->next;
   // }
+
+  // printf("%ld ",size); // 各要求サイズの出力を行って、binの範囲を考える
   
   // Best-fit
   my_metadata_t *best_metadata = NULL;
   my_metadata_t *best_prev = NULL;
-  while (metadata) {
-    if (size <= metadata->size){  // sizeよりも大きいmetadata->sizeが見つかった
-      if ( best_metadata == NULL || metadata->size < best_metadata->size){ // 必要なサイズ < metadataのサイズ < 今のベストサイズ のとき
-        best_metadata = metadata;
-        best_prev = prev;
+  for( int i = bin_index ; i < N ; i ++ ){
+    metadata = my_heap.free_head[i];
+    prev = NULL;
+
+    while (metadata) {
+      if (size <= metadata->size){  // sizeよりも大きいmetadata->sizeが見つかった
+        if ( best_metadata == NULL || metadata->size < best_metadata->size){ // 必要なサイズ < metadataのサイズ < 今のベストサイズ のとき
+          best_metadata = metadata;
+          best_prev = prev;
+        }
+      }
+      prev = metadata;
+      metadata = metadata->next;
     }
+    if (best_metadata){ // 見つかったとき
+      break;
     }
-    
-    prev = metadata;
-    metadata = metadata->next;
   }
+  
   prev = best_prev;
   metadata = best_metadata;
 
@@ -119,7 +148,7 @@ void *my_malloc(size_t size) {
     metadata->size = buffer_size - sizeof(my_metadata_t);
     metadata->next = NULL;
     // Add the memory region to the free list.
-    my_add_to_free_list(metadata);
+    my_add_to_free_list(metadata,calculate_bin_index(metadata->size));
     // Now, try my_malloc() again. This should succeed.
     return my_malloc(size);
   }
@@ -132,7 +161,7 @@ void *my_malloc(size_t size) {
   void *ptr = metadata + 1;
   size_t remaining_size = metadata->size - size;
   // Remove the free slot from the free list.
-  my_remove_from_free_list(metadata, prev);
+  my_remove_from_free_list(metadata, prev, calculate_bin_index(metadata->size));  //
 
   if (remaining_size > sizeof(my_metadata_t)) {
     // Shrink the metadata for the allocated object
@@ -152,7 +181,7 @@ void *my_malloc(size_t size) {
     new_metadata->size = remaining_size - sizeof(my_metadata_t);
     new_metadata->next = NULL;
     // Add the remaining free slot to the free list.
-    my_add_to_free_list(new_metadata);
+    my_add_to_free_list(new_metadata,calculate_bin_index(new_metadata->size));
   }
   return ptr;
 }
@@ -167,7 +196,7 @@ void my_free(void *ptr) {
   //     metadata   ptr
   my_metadata_t *metadata = (my_metadata_t *)ptr - 1;
   // Add the free slot to the free list.
-  my_add_to_free_list(metadata);
+  my_add_to_free_list(metadata,calculate_bin_index(metadata->size));
 }
 
 // This is called at the end of each challenge.
